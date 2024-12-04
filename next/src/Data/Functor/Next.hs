2c1c01e60164a0e25a7057f724475584c7f4c95b
r-sta <151573323+munston@users.noreply.github.com>
2024-12-04 06:17:30 +0000

    Update Next.hs

--- next/src/Data/Functor/Next.hs
+++ next/src/Data/Functor/Next.hs
@@ -1,4 +1,4 @@
-{-# Language MultiParamTypeClasses , FunctionalDependencies #-}
+{-# Language MultiParamTypeClasses , FunctionalDependencies , FlexibleInstances #-}
 module Data.Functor.Next where
 
 {-
@@ -10,6 +10,15 @@ class Next ix a | a -> ix where
  cursor :: a -> ix
  nudge :: (ix -> ix,(ix -> ix) -> a -> a)
 
+-- length, cursor, entries.
+data ListCursor a = ListCursor Int Int [a] deriving Show
+
+newListCursor xs = ListCursor 0 0 xs
+
+instance Next Int (ListCursor a) where
+ cursor (ListCursor _ ix _) = ix
+ nudge = ((+1),\f (ListCursor l ix xs) -> let (ix',ix'') = (f ix,if ix' == l then 0 else ix') in ListCursor l ix'' xs)
+
 next :: Next ix a => a -> a
 next a = g f a
  where
@@ -20,6 +29,11 @@ class Next ix b => Zipper ix a b | b -> a, b -> ix where
  getZ :: ix -> b -> a
  setZ :: ix -> a -> b -> b
 
+instance Zipper Int Int (ListCursor Int) where
+ lengthZ (ListCursor l ix xs) = l
+ getZ ix (ListCursor l _ xs) = xs !! ix
+ setZ ix a (ListCursor l ix' xs) | ix == ix' = ListCursor l ix' $ take ix xs ++ [ {-head (drop ix xs)-} a ] ++ tail (drop ix xs)
+
 editZ :: Zipper ix a b => (a->a) -> (b->b)
 editZ f x = z
  where
@@ -29,3 +43,5 @@ editZ f x = z
 
 monoMap :: Zipper ix a (f a) => (a->a) -> f a -> f a
 monoMap f x = iterate (next . editZ f) x !! (lengthZ x) 
+
+test = monoMap (error.show::Int->Int) $ newListCursor [1,2,3]
